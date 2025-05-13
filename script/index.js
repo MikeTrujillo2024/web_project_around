183894aa938efcb8eb5585785487ab962a5ad5fd
MikeTrujillo <trujillomike121986@gmail.com>
2025-05-13 14:52:30 -0600

    terminó de dar formato a front end de el modulo popu

---
+++ script/index.js
@@ -0,0 +1,13 @@
+// script.js
+
+const openFormButton = document.querySelector(".profile__info--edit_button");
+console.log(openFormButton);
+/* const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close-button");
+
+function toggleForm() {
+  popup.classList.toggle("popup_visible");
+}
+
+openFormButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm); */
