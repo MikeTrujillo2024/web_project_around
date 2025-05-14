ec0c3cfc5ab8015bf89a5db1d497711e465bd497
MikeTrujillo <trujillomike121986@gmail.com>
2025-05-13 22:19:00 -0600

    agregamos fucion de abrir y cerrar modal

--- script/index.js
+++ script/index.js
@@ -1,13 +1,48 @@
 // script.js
 
-const openFormButton = document.querySelector(".profile__info--edit_button");
-console.log(openFormButton);
-/* const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close-button");
+const openFormButton = document.querySelector(".content");
+const getinfo = openFormButton.querySelector(".profile__info--edit_button");
+const closeModalButton = document.querySelector(".popup");
 
-function toggleForm() {
-  popup.classList.toggle("popup_visible");
+/* open modal */
+function deleteClass(nombre,puesto) {
+  const classModal = document.querySelector(".popup_opened");
+  let nameModal = classModal.querySelector(".popup__content-name");
+  let aboutModal = classModal.querySelector(".popup__content-about");
+
+  nameModal.value=nombre;
+  aboutModal.value=puesto;
+
+  classModal.classList.remove("popup_opened");
 }
 
-openFormButton.addEventListener("click", toggleForm);
-closeButton.addEventListener("click", toggleForm); */
+
+
+function getinfoCostumer() {
+  let name = document.querySelector(".profile__info--name");
+  let puesto = document.querySelector(".profile__about");
+
+  if(name.textContent.length != 0 && puesto.textContent.length != 0){
+    deleteClass(name.textContent,puesto.textContent);
+
+  }else{
+    alert("Error con el nombre");
+  }
+}
+
+
+getinfo.addEventListener("click", getinfoCostumer);
+
+/* close modal */
+function closeModal(){
+  const classModal = document.querySelector(".popup");
+  let nameModal = classModal.querySelector(".popup__content-name");
+  let aboutModal = classModal.querySelector(".popup__content-about");
+  nameModal.value='';
+  aboutModal.value='';
+  classModal.classList.add("popup_opened");
+}
+
+
+closeModalButton.addEventListener("click",closeModal);
+
