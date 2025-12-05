3cb9adb602a3e24510772e36137266e04fdd779b
MikeTrujillo <trujillomike121986@gmail.com>
2025-12-04 22:12:49 -0600

    mejoramos la logica hicimos un codigo mas limpio y agregamos fuiciones para abrir y cerrar modals crear cards y guardar datso

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,4 @@
-import{ reset } from "./formvalidator.js";
-import { add } from "./index.js";
+import { CreateCard } from "./Card.js";
 /**
  * este objeto contiene las principales
  * selectores que usa para las validaciones
@@ -17,119 +16,164 @@ const settingsValidator = {
   formbuttonSubmit: ".popup__content-save",
 };
 
+let newImg = [];
+/**formulario editar profile */
 const openFormButton = document.querySelector(".content");
 const getinfo = openFormButton.querySelector(".profile__info_edit_button");
 const nameProfile = document.querySelector(".profile__name");
 const aboutProfile = document.querySelector(".profile__about");
 
 const popupProfile = document.querySelector("#popup-profile");
+const formElement = popupProfile.querySelector(".popup__container");
 const closeModalButton = popupProfile.querySelector(".popup__button-cancel");
 const nameInfo = popupProfile.querySelector(".popup__content-name");
 const aboutInfo = popupProfile.querySelector(".popup__content-about");
 
+/**nuevo lugar  */
 const popupAddCard = document.querySelector("#popup-places");
 const buttonAddCard = document.querySelector(".profile__info-button-add");
-const closeCancelButtonAddCard = popupAddCard.querySelector("#popup__button-cancelForm");
+const closeCancelButtonAddCard = popupAddCard.querySelector(
+  "#popup__button-cancelForm"
+);
 const saveButtonAddCard = popupAddCard.querySelector("#create");
 const namenewImg = popupAddCard.querySelector(".popup__content-name");
 const linknewImg = popupAddCard.querySelector(".popup__content-about");
-
+/**form image */
 const popupImagen = document.querySelector("#popup-image");
 const srcImgPopup = popupImagen.querySelector(".popup__img");
 const titleImgPopup = popupImagen.querySelector(".popup__img-title");
 const closemodalImg = popupImagen.querySelector("#popup__button-cancel-imagen");
+/** reser formularios */
+const resetValidation = (modal) => {
+  const formElments = modal.querySelectorAll(settingsValidator.formSelector);
+  formElments.forEach((formElment) => {
+    formElment.reset();
+  });
+};
 
+/* imagen modal */
 /* open modal Imagen */
 const openPopupImagen = (link, name) => {
-  popupImagen.classList.remove("popup_opened");
-  popupImagen.classList.add("mOpen");
+  openModal(popupImagen);
   srcImgPopup.src = link;
   srcImgPopup.alt = name;
   titleImgPopup.textContent = name;
 };
 
-/* open modal */
+/**cerrar cuando se presiona x de cacelar en modal de imagen */
+closemodalImg.addEventListener("click", () => {
+  closeModalAll("popup-image");
+});
+
+/* new Card */
+/* open modal Card */
+buttonAddCard.addEventListener("click", () => {
+  openModal(popupAddCard);
+});
+
+/* cerrar modal cuando se presiona x de cerrar */
+closeCancelButtonAddCard.addEventListener("click", () => {
+  closeModalAll("popup-places");
+});
+
+/* cuando se presiona el boton de guardar un nuevo lugar */
+saveButtonAddCard.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  newImg.push({
+    name: namenewImg.value,
+    link: linknewImg.value,
+  });
+
+  newImg.forEach((elment) => {
+    const cardAdd = new CreateCard(elment, "#card-template");
+    const cardNewElement = cardAdd.getCreateCard();
+    document.querySelector("#place").prepend(cardNewElement);
+  });
+  newImg.length = 0;
+  resetValidation(popupAddCard);
+  closeModalAll("popup-places");
+});
+
+/**cerrar cuando presionamos el boton de scape */
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    identifyModalOpen();
+  }
+});
+
+/**modal info User */
+getinfo.addEventListener("click", handleOpenProfilePopup);
+
+/**presionar el boton de guardar nueva info del usuario */
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+/**cuando se presioan el boton de cancelar */
+closeModalButton.addEventListener("click", () => {
+  closeModalAll("popup-profile");
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = formElement.querySelector(".popup__content-name");
+  let jobInput = formElement.querySelector(".popup__content-about");
+
+  nameProfile.textContent = nameInput.value;
+  aboutProfile.textContent = jobInput.value;
+
+  closeModalAll("popup-profile");
+}
+
+/**cambia las clases para cerrar el modal */
+function closeModalAll(idModal) {
+  document.getElementById(idModal).classList.add("popup_opened");
+  document.getElementById(idModal).classList.remove("mOpen");
+}
+
+/* open modal infoUser*/
 function handleOpenProfilePopup() {
   nameInfo.value = nameProfile.textContent;
   aboutInfo.value = aboutProfile.textContent;
-  popupProfile.classList.remove("popup_opened");
-  popupProfile.classList.add("mOpen");
+  openModal(popupProfile);
 }
 
-/* constante que sirve apara reseatear los modales */
-const closeModalAll = (classIdModal) => {
-  const classIdName = classIdModal;
-  classIdName.classList.add("popup_opened");
-  classIdName.classList.remove("mOpen");
-  reset(popupAddCard);
-};
-
-/* close modal */
-function closeModal() {
-  nameInfo.value = "";
-  aboutInfo.value = "";
-  closeModalAll(popupProfile);
-  reset(popupAddCard);
+/** cambia las clases para abrir el modal */
+function openModal(idModalOpen) {
+  idModalOpen.classList.remove("popup_opened");
+  idModalOpen.classList.add("mOpen");
 }
-
-/* cierra el modal después de dar click en el boton de guardar o de cerrar */
-function handleClosePopup() {
-  closeModalAll(popupAddCard);
-}
-
-/* close modal Imagen */
-function handleOpenImagePopup() {
-  closeModalAll(popupImagen);
-}
-
-/* open modal new form */
-function handleOpenFormPlacePopup() {
-  popupAddCard.classList.remove("popup_opened");
-  popupAddCard.classList.add("mOpen");
-}
-
-/* cuando de  */
- const handleSaveNewPlace =()=>{
-  const newImg = [
-    {
-    name: namenewImg.value,
-    link: linknewImg.value
-    }
-  ]
-
-  add(newImg,"#card-template");
-    handleClosePopup();
-}
-
-
-getinfo.addEventListener("click", handleOpenProfilePopup);
-buttonAddCard.addEventListener("click", handleOpenFormPlacePopup);
-closeModalButton.addEventListener("click",closeModal);
-closemodalImg.addEventListener("click", handleOpenImagePopup);
-closeCancelButtonAddCard.addEventListener("click",handleClosePopup);
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    closeModal();
-    handleClosePopup();
-    handleOpenImagePopup();
-  } else if (e.key === "Enter") {
-    handleProfileFormSubmit;
-    handleSaveNewPlace;
-  }
-});
-
 /* cerrar el modal click en cualquier parte del documento */
 document.addEventListener("click", function (e) {
   const popupclassModalClose = e.target.classList;
+  const popupidModal = e.target.id;
   if (popupclassModalClose.contains("mOpen")) {
-    closeModal();
-    handleClosePopup();
-    handleOpenImagePopup();
+    closeModalAll(popupidModal);
   }
 });
 
-/* boton para crear otra card */
-saveButtonAddCard.addEventListener("click",handleSaveNewPlace)
+/**recorremos todos los elmentos del dom que contengan el id popup y
+ * debemos encontrar la clase que contenga laclase mopen
+ * una vez que se encuentra se identifica el id y con este podemos
+ * cerrar el modal
+ */
+const identifyModalOpen = () => {
+  const formList = Array.from(document.getElementsByClassName("popup"));
+  const modalOpen = formList.find((open) => {
+    return open.classList.contains("mOpen");
+  });
+  if (modalOpen) {
+    switch (modalOpen.id) {
+      case "popup-places":
+        resetValidation(popupAddCard);
+        break;
+      case "popup-profile":
+        resetValidation(popupProfile);
+        break;
 
-
-export { openPopupImagen as img,settingsValidator }
+      default:
+        console.log(`(=^・^=)`);
+    }
+    closeModalAll(modalOpen.id);
+  }
+};
+export { openPopupImagen as img, settingsValidator };
