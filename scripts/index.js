cd7990dd9f7faa0a182abfb1b3ea1be5124ff1e6
Mike Trujillo <trujillomike121986@gmail.com>
2026-01-28 21:58:12 -0600

    mejoramos estructura de la calse index y renderizamos todas las clases

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,22 @@
-import Section  from "./Section.js";
-import { Card } from "./Card.js";
-import PopupWithImage from "./PopupWithImage.js";
-/* import { settingsValidator } from "./utils.js"; */
+import Section from "./Section.js";
+import Card from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
+import UserInfo from "./UserInfo.js";
+import PopupWithImage from "./PopupWithImage.js";
+import PopupWithForm from "./popupwithform.js";
+import {
+  initialCards,
+  settingsValidator,
+  getinfo,
+  addimg
+} from "./utils.js";
+
 
 /**
  * constiene todods los fiormularios
  */
 /* const formElements = document.querySelectorAll(settingsValidator.formSelector); */
 
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
 
 /***
  * hacemos que se muestra los popup de imagenes
@@ -42,34 +24,82 @@ const initialCards = [
 const popupImage = new PopupWithImage("#popup-image");
 popupImage.setEventListeners();
 
-const handleCardClick = (link,name) =>{
-  popupImage.open(link,name)
+const handleCardClick = (link, name) => {
+  popupImage.open(link, name)
 }
+
+
 /**
  * mostramos los card desde un principio
  */
-
-
 const cardList = new Section(
   {
-  items:initialCards,
-  renderer: (item)=>{
-    const cardItem = new Card(item,"#card-template",handleCardClick);
-    cardList.addItem(cardItem.getCreateCard());
-  }
-},"#place");
+    items: initialCards,
+    renderer: (item) => {
+      const cardItem = new Card(item, "#card-template", handleCardClick);
+      cardList.addItem(cardItem.getCreateCard());
+    }
+  }, "#place");
 
 cardList.rendererItems();
 
+/**
+ * inicializamos user info
+ */
+const user = new UserInfo({ selectorName: ".profile__name", selectorAbout: ".profile__about" });
 
-/* initialCards.forEach((items) => {
-  const cardItem = new Card(items, "#card-template");
-  const cardElement = cardItem.getCreateCard();
+/**
+ * inicializamos popupwith form para editar el formularios
+ */
+const profilePopup = new PopupWithForm({
+  selector: "#popup-profile",
+  submitCallback: (formData) => {
+    user.setUserInfo({
+      name: formData.name__user,
+      about: formData.about
+    });
+    profilePopup.close();
+  }
+});
 
-  document.querySelector("#place").prepend(cardElement);
-}); */
+profilePopup.setEventListeners();
 
-/* formElements.forEach((formElement) => {
-  const formValid = new FormValidator(settingsValidator, formElement);
-  formValid.enableValidation();
-}); */
+/**
+ * abrir modal de edit user info
+ */
+getinfo.addEventListener("click", () => {
+  const { name, about } = user.getUserInfo();
+
+  document.querySelector("#input__popup_name_Editar").value = name;
+  document.querySelector("#input_popup_about").value = about;
+  profilePopup.open()
+});
+
+/**
+ * abrir el modal para agregar una nueva imagen
+ */
+
+const addNewImage = new PopupWithForm({
+  selector: "#popup-places",
+  submitCallback: (formData) => {
+    const newCard = new Card(
+      {
+        name: formData.titulo,
+        link: formData.url
+      },
+      "#card-template",
+      handleCardClick
+
+    );
+    cardList.addItem(newCard.getCreateCard());
+    addNewImage.close();
+  }
+});
+
+addNewImage.setEventListeners();
+/* console.log(addimg); */
+
+addimg.addEventListener("click", () => {
+  addNewImage.open();
+})
+
